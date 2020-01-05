a00c69fceae48b172d0403386ae0c0547b906733
Joe DiSalvo <joedisalvo04@gmail.com>
2020-01-05 18:24:27 -0500

    First commit

---
+++ lease-mileage.js
@@ -0,0 +1,18 @@
+const leaseStartDate = (new Date(2018,02,21)).valueOf();
+const totalLeaseDays = 365 * 3;
+const totalLeaseMiles = 12000 * 3;
+const milesPerDayAllowed = totalLeaseMiles / totalLeaseDays;
+
+const currentDate = (new Date()).valueOf();
+const leaseMilesUsed = prompt('Current Mileage?');
+const leaseDaysUsed = (currentDate - leaseStartDate) / (24 * 60 * 60 * 1000);
+const leaseDaysLeft = totalLeaseDays - leaseDaysUsed;
+const leaseMilesLeft = totalLeaseMiles - leaseMilesUsed;
+
+const targetLeaseMiles = milesPerDayAllowed * leaseDaysUsed;
+const milesOverLimit = leaseMilesUsed - targetLeaseMiles;
+const milesPerDayAdjusted = leaseMilesLeft / leaseDaysLeft;
+
+console.log(totalLeaseMiles / totalLeaseDays)
+console.log("Miles over limit to date: " + milesOverLimit);
+console.log("Adjusted allowed miles per day: " + milesPerDayAdjusted);
