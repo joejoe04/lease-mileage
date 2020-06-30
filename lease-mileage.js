8b2cafe253f9722be83c09676e1db7a7f0d47c32
Joe DiSalvo <joedisalvo04@gmail.com>
2020-06-30 16:17:42 -0500

    fixed margin for results and error messages, and fixed bug where error message was not being removed upon successful form submission

--- lease-mileage.js
+++ lease-mileage.js
@@ -11,25 +11,25 @@ const createLeaseCalcHTML = (e) => {
     '<div class="lc-ctnr">',
     '<h2>Lease Mileage Calculator</h2>',
       '<form action="" id="lc-form" class="lc-form">',
-        '<div class="lc-input-ctnr">',
-          '<label for="lc-start-date">Lease Start Date (MM/DD/YYYY)</label>',
-          '<input type="date" name="lc-start-date" id="lc-start-date" value="2018-03-18">',
+        '<div id="lc-start-date-ctr" class="lc-input-ctnr">',
+          '<label for="lc-start-date">Lease Start Date</label>',
+          '<input type="date" name="lc-start-date" id="lc-start-date" value="" placeholder="03/17/2018">',
         '</div>',
-        '<div class="lc-input-ctnr">',
+        '<div id="lc-duration-years-ctr" class="lc-input-ctnr">',
           '<label for="lc-duration-years">Lease Duration (years)</label>',
-          '<input type="text" name="lc-duration-years" id="lc-duration-years">',
+          '<input type="text" name="lc-duration-years" id="lc-duration-years" placeholder="3">',
         '</div>',
-        '<div class="lc-input-ctnr">',
+        '<div id="lc-miles-per-year-ctr" class="lc-input-ctnr">',
           '<label for="lc-miles-per-year">Miles Allowed Per Year</label>',
-          '<input type="text" name="lc-miles-per-year" id="lc-miles-per-year" value="12000">',
+          '<input type="text" name="lc-miles-per-year" id="lc-miles-per-year" value="" placeholder="12000">',
         '</div>',
-        '<div class="lc-input-ctnr">',
-          '<label for="lc-current-mileage">Current Mileage on Car</label>',
-          '<input type="text" name="lc-current-mileage" id="lc-current-mileage" value="25740">',
+        '<div id="lc-current-mileage-ctr" class="lc-input-ctnr">',
+          '<label for="lc-current-mileage">Current Mileage</label>',
+          '<input type="text" name="lc-current-mileage" id="lc-current-mileage" value="" placeholder="25700">',
         '</div>',
-        '<div class="lc-input-ctnr">',
-          '<label for="lc-mileage-before-lease">Miles on car before lease started</label>',
-          '<input type="text" name="lc-mileage-before-lease" id="lc-mileage-before-lease" value="400">',
+        '<div id="lc-mileage-before-lease-ctr" class="lc-input-ctnr lc-input-ctnr--last">',
+          '<label for="lc-mileage-before-lease">Mileage before lease start</label>',
+          '<input type="text" name="lc-mileage-before-lease" id="lc-mileage-before-lease" value="" placeholder="400">',
         '</div>',
       '<input class="lc-lease-calc-trigger lc-submit" type="submit" id="lc-submit" value="Calculate"></input>',
       '</form>',
@@ -37,8 +37,9 @@ const createLeaseCalcHTML = (e) => {
         '<p class="lc-miles-over-under" id="lc-miles-over-under"></p>',
         '<p class="lc-orig-miles-per-day" id="lc-orig-miles-per-day"></p>',
         '<p class="lc-adjusted-miles-per-day" id="lc-adjusted-miles-per-day"></p>',
+        '<p class="lc-form-invalid-msg" id="lc-form-invalid-msg"></p>',
       '</div>',
-      '<div class="lc-close"></div>',
+      '<div id="lc-close" class="lc-close"></div>',
     '</div>',
   ].join('');
 
@@ -49,6 +50,15 @@ const createLeaseCalcHTML = (e) => {
   lcDarken.innerHTML = leaseCalcHTML;
   lcFrag.appendChild(lcDarken);
   document.body.appendChild(lcFrag);
+
+  document.getElementById('lc-start-date').addEventListener('blur', validateStartDate, false);
+  document.getElementById('lc-duration-years').addEventListener('blur', validateDuration, false);
+  document.getElementById('lc-miles-per-year').addEventListener('blur', validateMilesPerYear, false);
+  document.getElementById('lc-current-mileage').addEventListener('blur', validateCurrentMileage, false);
+  document.getElementById('lc-mileage-before-lease').addEventListener('blur', validateMileageBeforeLease, false);
+
+  document.getElementById('lc-darken').addEventListener('click', closeLeaseMileage, false);
+  document.getElementById('lc-close').addEventListener('click', closeLeaseMileage, false);
 
   const leaseCalcTriggers = document.getElementsByClassName('lc-lease-calc-trigger');
 
@@ -65,14 +75,123 @@ for (let i = 0; i < leaseDisplayTriggers.length; i++) {
   leaseDisplayTriggers[i].addEventListener('click', createLeaseCalcHTML);
 }
 
+const validateStartDate = () => {
+
+  const field = document.getElementById('lc-start-date');
+  const fieldParent = field.parentElement;
+  const val = field.value;
+  const regex = /^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/g;
+  const isValid = regex.test(val);
+
+  isValid ? fieldParent.classList.remove('lc-not-valid') : fieldParent.classList.add('lc-not-valid');
+  
+  return isValid;
+};
+
+const validateDuration = () => {
+
+  const field = document.getElementById('lc-duration-years');
+  const fieldParent = field.parentElement;
+  const trimRegex = /[.,-\s]/g;
+  const val = field.value.replace(trimRegex, '');
+  field.value = val;
+  const regex = /^[0-9]{1}$/g;
+  const isValid = regex.test(val);
+
+  isValid ? fieldParent.classList.remove('lc-not-valid') : fieldParent.classList.add('lc-not-valid');
+  
+  return isValid;
+};
+
+const validateMilesPerYear = () => {
+
+  const field = document.getElementById('lc-miles-per-year');
+  const fieldParent = field.parentElement;
+  const trimRegex = /[.,-\s]/g;
+  const val = field.value.replace(trimRegex, '');
+  field.value = val;
+  const regex = /^[0-9]+$/g;
+  const isValid = regex.test(val);
+
+  isValid ? fieldParent.classList.remove('lc-not-valid') : fieldParent.classList.add('lc-not-valid');
+  
+  return isValid;
+};
+
+const validateCurrentMileage = () => {
+
+  const field = document.getElementById('lc-current-mileage');
+  const fieldParent = field.parentElement;
+  const trimRegex = /[.,-\s]/g;
+  const val = field.value.replace(trimRegex, '');
+  field.value = val;
+  const regex = /^[0-9]+$/g;
+  const isValid = regex.test(val);
+
+  isValid ? fieldParent.classList.remove('lc-not-valid') : fieldParent.classList.add('lc-not-valid');
+  
+  return isValid;
+};
+
+const validateMileageBeforeLease = () => {
+
+  const field = document.getElementById('lc-mileage-before-lease');
+  const fieldParent = field.parentElement;
+  const trimRegex = /[.,-\s]/g;
+  const val = field.value.replace(trimRegex, '');
+  field.value = val;
+  const regex = /^[0-9]+$/g;
+  const isValid = regex.test(val);
+
+  isValid ? fieldParent.classList.remove('lc-not-valid') : fieldParent.classList.add('lc-not-valid');
+  
+  return isValid;
+};
+
+
+const validateForm = () => {
+
+  const testResults = [];
+  let formIsValid = true;
+
+  testResults.push(validateStartDate());
+  testResults.push(validateDuration());
+  testResults.push(validateMilesPerYear());
+  testResults.push(validateCurrentMileage());
+  testResults.push(validateMileageBeforeLease());
+
+  for (let i = 0; i < testResults.length; i++) {
+
+    if (testResults[i] === false) {
+
+      formIsValid = false;
+
+      return formIsValid;
+    }
+  }
+};
+
 
 
 const calculateLeaseInfo = (e) => {
 
   e.preventDefault();
 
+  const formIsValid = validateForm();
+
+  if (false === formIsValid) {
+
+    document.getElementById('lc-miles-over-under').innerText = '';
+    document.getElementById('lc-orig-miles-per-day').innerText = '';
+    document.getElementById('lc-adjusted-miles-per-day').innerText = '';
+
+    document.getElementById('lc-form-invalid-msg').innerText = 'One or more of the form fields have been filled out incorrectly. Please follow the suggestions and then try again.';
+
+    return;
+  }
+
   const leaseStartDate = new Date(document.getElementById('lc-start-date').value).valueOf();
-  const leaseDurationYears = parseInt(document.getElementById('lc-duration-years').value, 10);
+  const leaseDurationYears = parseFloat(document.getElementById('lc-duration-years').value);
   const leaseTotalDays = 365 * leaseDurationYears;
   const leaseMilesPerYear = parseInt(document.getElementById('lc-miles-per-year').value, 10);
   const leaseTotalMiles = leaseMilesPerYear * leaseDurationYears;
@@ -112,4 +231,16 @@ const calculateLeaseInfo = (e) => {
   const leaseAdjustedMilesPerDayMsg = 'Given your current mileage, you can drive an average of ' + leaseAdjustedMilesPerDay.toFixed(2) + ' miles per day and return your car at your lease\'s mileage limit.';
 
   document.getElementById('lc-adjusted-miles-per-day').innerText = leaseAdjustedMilesPerDayMsg;
+
+  document.getElementById('lc-form-invalid-msg').innerText = '';
+};
+
+const closeLeaseMileage = (e) => {
+
+  const close = document.getElementById('lc-close');
+  const darken = document.getElementById('lc-darken');
+
+  if (e.target !== close && e.target !== darken) { return; }
+
+  document.body.removeChild(document.getElementById('lc-darken'));
 };
